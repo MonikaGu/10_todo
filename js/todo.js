f178d13e564f9ca9722d5a847867a1046298b8bb
MonikaGu <63239253+MonikaGu@users.noreply.github.com>
2020-05-06 12:25:07 +0300

    todo list

---
+++ js/todo.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let todo_list = [
+    [`Sportuoti`, `2020-05-05 15:01`, `2020-05-06 17:35`, `todo`],
+    [`Valgyti`, `2020-05-05 17:36`, `2020-05-05 18:00`, `todo`],
+    [`Eiti pasivaikscioti`, `2020-05-05 18:15`, `2020-05-06 20:00`, `todo`],
+];
+
+console.log(todo_list);
